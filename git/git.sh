3a0ecc42e9290ead7a153d2bb89f658ce50c953f
Adam Bourg <Adam.Bourg@gmail.com>
2016-02-21 17:44:12 -0700

    Updating the git commit

--- git/git.sh
+++ git/git.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+
+echo 'Entering git cli'
+
 # Copy is default vs config
 if [[ $1 == "--config" ]]; then
+  echo 'installing config'
   cp .gitconfig ~/
 else
+  echo 'copying config'
   cp ~/.gitconfig .
   git add .;
   git commit -m 'Updating the git commit';
